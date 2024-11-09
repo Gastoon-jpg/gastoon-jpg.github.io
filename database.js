801e8812fef59d5673d8e8af6a1a4d84b7c62fef
Gastoon <74118841+Gastoon-jpg@users.noreply.github.com>
2024-11-09 14:13:46 -0300

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 // database.js
 const videoDatabase = [
   {
-    thumbnail: "https://images.app.goo.gl/AJwKc18Xsrx1cFP77",
-    videoUrl: "https://www.youtube.com/embed/5NV6Rdv1a3I"
+    thumbnail: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQhC20GHWM4j9Ls2A4sm6UlNzY__6EnvYJd-tnRqX1qxuuwIblK2jvOClCBJ_1aPLI69R9v0SsQDtWwmCKeK4vUtBlfq3lW6iVx82M_PQ",
+    videoUrl: "https://asdas1241-gravityia.hf.space/stream.m3u8"
   },
   {
     thumbnail: "https://images.app.goo.gl/emqhvARFFaHmCPvy5",
