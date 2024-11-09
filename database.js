5c5721c6df9b2073b37c8395275d64e05529ad2f
Gastoon <74118841+Gastoon-jpg@users.noreply.github.com>
2024-11-09 13:42:21 -0300

    Update database.js

--- database.js
+++ database.js
@@ -2,11 +2,11 @@
 const videoDatabase = [
   {
     thumbnail: "https://images.app.goo.gl/AJwKc18Xsrx1cFP77",
-    videoUrl: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    videoUrl: "https://www.youtube.com/embed/5NV6Rdv1a3I"
   },
   {
     thumbnail: "https://images.app.goo.gl/emqhvARFFaHmCPvy5",
-    videoUrl: "https://www.youtube.com/embed/3JZ_D3ELwOQ"
+    videoUrl: "https://www.youtube.com/embed/5NV6Rdv1a3I"
   },
   {
     thumbnail: "https://images.app.goo.gl/qPwA7FZ417NUB3RY9",
